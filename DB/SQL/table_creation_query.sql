294ccaaa1d65449a506632792dbee9fcd6d27a07
ggwplarin <39963694+ggwplarin@users.noreply.github.com>
2021-01-14 23:42:02 +0300

    Fixed query file.
    text changed to VARCHAR(MAX)

--- DB/SQL/table_creation_query.sql
+++ DB/SQL/table_creation_query.sql
@@ -32,52 +32,52 @@ GO
 
 CREATE TABLE shop_users
 (
-  shop_user_id       int  NOT NULL,
-  shop_user_login    text NOT NULL,
-  shop_user_password text NOT NULL,
+  shop_user_id       INT  NOT NULL IDENTITY,
+  shop_user_login    VARCHAR(MAX) NOT NULL,
+  shop_user_password VARCHAR(MAX) NOT NULL,
   CONSTRAINT PK_users PRIMARY KEY (shop_user_id)
 )
 GO
 CREATE TABLE cashbox
 (
-  sale_id                 int      NOT NULL,
-  sale_date               datetime NOT NULL,
-  sale_setout_position_id int      NOT NULL,
-  sale_acquirer_id        int      NOT NULL,
+  sale_id                 INT      NOT NULL IDENTITY,
+  sale_date               DATETIME NOT NULL,
+  sale_setout_position_id INT      NOT NULL,
+  sale_acquirer_id        INT      NOT NULL,
   CONSTRAINT PK_cashbox PRIMARY KEY (sale_id)
 )
 GO
 CREATE TABLE setout
 (
-  setout_position_id      int NOT NULL,
-  setout_position_price   int NOT NULL,
-  setout_antique_thing_id int NOT NULL,
+  setout_position_id      INT NOT NULL IDENTITY,
+  setout_position_price   INT NOT NULL,
+  setout_antique_thing_id INT NOT NULL,
   CONSTRAINT PK_setout PRIMARY KEY (setout_position_id)
 )
 GO
 CREATE TABLE antiques 
 (
-  antique_thing_id                 int  NOT NULL,
-  antique_thing_title              text NOT NULL,
-  antique_thing_description        text NOT NULL DEFAULT '-',
-  antique_thing_storage_conditions text NOT NULL DEFAULT '-',
-  antique_thing_story_id           int  NOT NULL,
+  antique_thing_id                 INT  NOT NULL IDENTITY,
+  antique_thing_title              VARCHAR(MAX) NOT NULL,
+  antique_thing_description        VARCHAR(MAX) NOT NULL DEFAULT '-',
+  antique_thing_storage_conditions VARCHAR(MAX) NOT NULL DEFAULT '-',
+  antique_thing_story_id           INT  NOT NULL,
   CONSTRAINT PK_antiques  PRIMARY KEY (antique_thing_id)
 )
 GO
 CREATE TABLE story_archive
 (
-  story_id      int  NOT NULL,
-  story_title   text NOT NULL,
-  story_content text NOT NULL,
+  story_id      INT  NOT NULL IDENTITY,
+  story_title   VARCHAR(MAX) NOT NULL,
+  story_content VARCHAR(MAX) NOT NULL,
   CONSTRAINT PK_story_archive PRIMARY KEY (story_id)
 )
 GO
 CREATE TABLE acquirers
 (
-  acquirer_id         int  NOT NULL,
-  acquirer_first_name text NOT NULL,
-  acquirer_last_name  text NOT NULL,
+  acquirer_id         INT  NOT NULL IDENTITY,
+  acquirer_first_name VARCHAR(MAX) NOT NULL,
+  acquirer_last_name  VARCHAR(MAX) NOT NULL,
   CONSTRAINT PK_acquirers PRIMARY KEY (acquirer_id)
 )
 GO
@@ -101,4 +101,7 @@ ALTER TABLE cashbox
   ADD CONSTRAINT FK_acquirers_TO_cashbox
     FOREIGN KEY (sale_acquirer_id)
     REFERENCES acquirers (acquirer_id)
+GO
+
+INSERT INTO shop_users (shop_user_login,shop_user_password) VALUES ('SenjorAdmin','!2#qWeRtY1@3')
 GO
