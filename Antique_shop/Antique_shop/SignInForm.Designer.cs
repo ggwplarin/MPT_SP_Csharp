1bc671b2ac6038b9974e01c82695ec08bdf2ea4c
ggwplarin <39963694+ggwplarin@users.noreply.github.com>
2021-01-14 19:02:54 +0300

    Added project files.

---
+++ Antique_shop/Antique_shop/SignInForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Antique_shop
+{
+    partial class SignInForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_login = new System.Windows.Forms.TextBox();
+            this.btn_signIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(12, 44);
+            this.tb_password.MaxLength = 32;
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PlaceholderText = "Пароль";
+            this.tb_password.Size = new System.Drawing.Size(182, 23);
+            this.tb_password.TabIndex = 1;
+            this.tb_password.UseSystemPasswordChar = true;
+            this.tb_password.WordWrap = false;
+            this.tb_password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_password_KeyPress);
+            // 
+            // tb_login
+            // 
+            this.tb_login.Location = new System.Drawing.Point(12, 12);
+            this.tb_login.MaxLength = 32;
+            this.tb_login.Name = "tb_login";
+            this.tb_login.PlaceholderText = "Логин";
+            this.tb_login.Size = new System.Drawing.Size(182, 23);
+            this.tb_login.TabIndex = 0;
+            this.tb_login.WordWrap = false;
+            this.tb_login.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_login_KeyPress);
+            // 
+            // btn_signIn
+            // 
+            this.btn_signIn.Location = new System.Drawing.Point(12, 74);
+            this.btn_signIn.Name = "btn_signIn";
+            this.btn_signIn.Size = new System.Drawing.Size(182, 23);
+            this.btn_signIn.TabIndex = 2;
+            this.btn_signIn.Text = "Войти";
+            this.btn_signIn.UseVisualStyleBackColor = true;
+            this.btn_signIn.Click += new System.EventHandler(this.btn_signIn_Click);
+            // 
+            // SignInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(206, 109);
+            this.Controls.Add(this.btn_signIn);
+            this.Controls.Add(this.tb_login);
+            this.Controls.Add(this.tb_password);
+            this.Name = "SignInForm";
+            this.Text = "Вход";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_login;
+        private System.Windows.Forms.Button btn_signIn;
+    }
+}
+
---
+++ Antique_shop/Antique_shop/SignInForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Antique_shop
+{
+    public partial class SignInForm : Form
+    {
+        public string ConStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Antique_shop_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+        public SignInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_login_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = e.KeyChar == (char)Keys.Space;
+        }
+        private void tb_password_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = e.KeyChar == (char)Keys.Space;
+        }
+        private void btn_signIn_Click(object sender, EventArgs e)
+        {
+            if (!SignInFieldsValidation(tb_login.Text, tb_password.Text))
+                MessageBox.Show(Properties.Strings.SignInForm_MB_SignInFieldsValidationFailed_text, Properties.Strings.SignInForm_MB_SignInFieldsValidationFailed_caption);
+            using (SqlCo)
+            {
+                
+            }
+        }
+
+        public bool SignInFieldsValidation(string login, string password)
+        {
+            if (string.IsNullOrWhiteSpace(login) & string.IsNullOrWhiteSpace(password)) return false;
+            const string symbols = "!@#$%^&?*()[]{}";
+            var loginOk = true;
+            var passwordOk = true;
+            loginOk &= !login.Contains(" ");
+            loginOk &= login.Length >= 6;
+            loginOk &= login.All(c => char.IsLetterOrDigit(c) || c == '_');
+            passwordOk &= !password.Contains(" ");
+            passwordOk &= password.Length >= 8;
+            passwordOk &= password.Any(char.IsDigit);
+            passwordOk &= password.Any(char.IsLetter);
+            passwordOk &= password.Any(char.IsUpper);
+            passwordOk &= password.Any(char.IsLower);
+            passwordOk &= password.Any(c => symbols.Contains(c));
+            return loginOk & passwordOk;
+        }
+
+
+    }
+}
